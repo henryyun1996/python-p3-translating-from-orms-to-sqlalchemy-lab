e0f738caf278e700669390a77af69494389b2be8
Henry <henrysungyun@gmail.com>
2023-03-28 00:37:35 -0700

    finish

--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer)
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Dog(Base):
-    pass
+    __tablename__ = 'dogs'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(10))
+    breed = Column(String(25))
+
